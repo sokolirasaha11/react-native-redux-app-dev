ab9ab797066854734113da5b99600ee2ef8891c3
sokolirasaha11 <sokolirasaha11@gmail.com>
2022-03-22 22:38:07 +0300

    Adding navigation
    Navigation has been added to the app.

---
+++ scr/navigation/mainNavigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { BodyScreen } from '../screens/BodyScreen';
+import { CreateNode } from '../screens/FavoritesBodyScreen';
+
+const Stack = createNativeStackNavigator();
+export function AppNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Body">
+                <Stack.Screen name="Body" component={BodyScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Create" component={CreateNode} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
